48c2dcf088272bfd1edd61c14e58d06e0ac3cb7b
avirathod <avinashrathod55252@gmail.com>
2023-10-24 16:22:19 +0530

    Density Exchange

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import type { NextPage } from "next";
+
+const Image1: NextPage = () => {
+  return (
+    <div className="relative w-full h-[8725px] bg-[url('/image@3x.png')] bg-cover bg-no-repeat bg-[top]" />
+  );
+};
+
+export default Image1;
